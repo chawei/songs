e339f9eea35de4bebc51581d5e6e5b80cab8dab2
David Chawei Hsu <chawei.hsu@gmail.com>
2011-02-21 14:51:08 -0800

    refactor js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,75 +2,8 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(function() {
   $('#container').removeClass('no_js').addClass('js');
-  
-  //$('#lyric_show_container form').hide();
-  
-  /*
-  $('#lyric_show_container .section .add_btn').click(function() {
-    var form = $(this).parents('.section').find('#add_new_form_container form');
-    if (form.hasClass('expanded')) {
-      form.removeClass('expanded').slideUp();
-    } else {
-      form.addClass('expanded').slideDown();
-    }
-    return false;
-  });
-  */
-  
-  $('.tab_content .add_btn').click(function() {
-    var form = $(this).parents('.tab_content').find('#add_new_form_container form');
-    if (form.hasClass('expanded')) {
-      form.removeClass('expanded').hide();
-    } else {
-      form.addClass('expanded').show();
-    }
-    return false;
-  });
-  
-  $('.tab_content .edit_btn').live('click', function() {
-    var form = $(this).parents('.item').find('.form_container form');
-    if (form.hasClass('expanded')) {
-      form.removeClass('expanded').hide();
-    } else {
-      form.addClass('expanded').show();
-    }
-    return false;
-  });
-  
-  $('.tab_content .cancel_btn').live('click', function() {
-    var cancel_btn = $(this);
-    cancel_btn.parents('form').removeClass('expanded').hide(function(){
-      var item = cancel_btn.parents('.item');
-      if(item.length > 0) {
-        item.find('.title').show();
-        item.find('.content').show();
-      }
-    });
-    return false;
-  });
-  
-  $('.tab_content .save_btn').live('click', function() {
-    var save_btn = $(this);
-    save_btn.val('saving..');
-    save_btn.submit();
-    return false;
-  });
-  
-  $('.edit_lyric_btn').live('click', function() {
-    if($(this).hasClass('click_to_edit')) {
-      $(this).removeClass('click_to_edit').addClass('click_to_hide');
-      $('#lyric').hide();
-      $('#lyric_form').removeClass('js_hidden').hide().fadeIn();
-      $(this).text('cancel');
-    } else if ($(this).hasClass('click_to_hide')) {
-      $(this).removeClass('click_to_hide').addClass('click_to_edit');
-      $('#lyric').fadeIn();
-      $('#lyric_form').addClass('js_hidden').hide();
-      $(this).text('edit the lyrics');
-    }
-    return false;
-  });
-  
+  $('#tabs').removeClass('js_hidden');
+
   $('.ajax_video').click(function() {
     var video_item = $(this).parents('.video_item')
     $('.video_item').removeClass('selected_video_item');
@@ -79,10 +12,7 @@ $(function() {
 
   $('[rel=tipsy]').tipsy({gravity: 'n'});
   //$('.vote_block .action').tipTip();
-  
-  $('.autogrow').autogrow();
-  
-  
+
   /* Vote Block */
   $('.vote_block .action').click(function(){
     var btn = $(this);
@@ -94,23 +24,4 @@ $(function() {
     }
   });
   
-  /* Stories */
-  $('#story_section form .title').watermark('Title (Optional)');
-  $('#story_section form .content').watermark('Start writing your story here...');
 });
-
-$.fn.clearForm = function() {
-  $(this).find('.msg').empty();
-  return this.each(function() {
-    var type = this.type, tag = this.tagName.toLowerCase();
-    if (tag == 'form')
-      return $(':input',this).clearForm();
-    if (type == 'text' || type == 'password' || tag == 'textarea') {
-      this.value = '';
-    }
-    else if (type == 'checkbox' || type == 'radio')
-      this.checked = false;
-    //else if (tag == 'select')
-    //  this.selectedIndex = -1;
-  });
-};
---
+++ public/javascripts/song.form.js
@@ -0,0 +1,92 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+$(function() {  
+  $('.tab_content .add_btn').click(function() {
+    var form = $(this).parents('.tab_content').find('#add_new_form_container form');
+    if (form.hasClass('expanded')) {
+      form.removeClass('expanded').hide();
+    } else {
+      form.addClass('expanded').show();
+    }
+    return false;
+  });
+  
+  $('.tab_content .edit_btn').live('click', function() {
+    var form = $(this).parents('.item').find('.form_container form');
+    if (form.hasClass('expanded')) {
+      form.removeClass('expanded').hide();
+    } else {
+      form.addClass('expanded').show();
+    }
+    return false;
+  });
+  
+  $('.tab_content .cancel_btn').live('click', function() {
+    var cancel_btn = $(this);
+    cancel_btn.parents('form').removeClass('expanded').hide(function(){
+      var item = cancel_btn.parents('.item');
+      if(item.length > 0) {
+        item.find('.title').show();
+        item.find('.content').show();
+      }
+    });
+    return false;
+  });
+  
+  $('.tab_content .save_btn').live('click', function() {
+    var save_btn = $(this);
+    var form = save_btn.parents('form');
+    var textarea = form.find('textarea');
+    if (textarea.val().length == 0) {
+      textarea.val("Please enter some text...").addClass('error');
+    } else {
+      save_btn.val('saving..');
+      save_btn.submit();
+    }
+    return false;
+  });
+  
+  $('.tab_content form textarea').live('click', function(){
+    var textarea = $(this);
+    if (textarea.hasClass('error')) {
+      textarea.val("").removeClass('error');
+    }
+  });
+  
+  $('.edit_lyric_btn').live('click', function() {
+    if($(this).hasClass('click_to_edit')) {
+      $(this).removeClass('click_to_edit').addClass('click_to_hide');
+      $('#lyric').hide();
+      $('#lyric_form').removeClass('js_hidden').hide().fadeIn();
+      $(this).text('cancel');
+    } else if ($(this).hasClass('click_to_hide')) {
+      $(this).removeClass('click_to_hide').addClass('click_to_edit');
+      $('#lyric').fadeIn();
+      $('#lyric_form').addClass('js_hidden').hide();
+      $(this).text('edit the lyrics');
+    }
+    return false;
+  });
+  
+  $('.autogrow').autogrow();
+  
+  /* Stories */
+  $('#story_section form .title').watermark('Title (Optional)');
+  $('#story_section form .content').watermark('Start writing your story here...');
+});
+
+$.fn.clearForm = function() {
+  $(this).find('.msg').empty();
+  return this.each(function() {
+    var type = this.type, tag = this.tagName.toLowerCase();
+    if (tag == 'form')
+      return $(':input',this).clearForm();
+    if (type == 'text' || type == 'password' || tag == 'textarea') {
+      this.value = '';
+    }
+    else if (type == 'checkbox' || type == 'radio')
+      this.checked = false;
+    //else if (tag == 'select')
+    //  this.selectedIndex = -1;
+  });
+};
