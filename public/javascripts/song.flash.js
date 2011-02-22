8d0ec7bf920a64bd87aa4937609e6f930a867571
David Chawei Hsu <chawei.hsu@gmail.com>
2011-02-21 17:08:03 -0800

    fix js bug

--- public/javascripts/song.flash.js
+++ public/javascripts/song.flash.js
@@ -1,6 +1,6 @@
 $(function() {
   var flash_msgs = $('#flash_msgs');
-  if (flash_msgs.html().length > 0) {
+  if (flash_msgs != undefined) {
     flash_msgs.find('div').delay(3000).fadeOut();
   }
 });
