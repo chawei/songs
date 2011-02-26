08b0d7bcc445f01094ef8ffcc8bcc4ebdda99b32
David Chawei Hsu <chawei.hsu@gmail.com>
2011-02-26 11:47:09 -0800

    fix bug

--- lib/extra/song_importer.rb
+++ lib/extra/song_importer.rb
@@ -15,6 +15,7 @@ class SongImporter
   
   
   def self.get_title_and_artist_name(query)
+    artist, title = nil, nil
     res = self.get("http://ws.audioscrobbler.com/2.0/?method=track.search&track=#{URI.escape(query)}&api_key=#{API_KEY}")
     if trackmatches = res['lfm']['results']['trackmatches']
       tracks = trackmatches['track']
