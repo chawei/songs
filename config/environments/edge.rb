8d707576f7fdb69ec6b4442dda469295224ef2c5
David Chawei Hsu <chawei.hsu@gmail.com>
2011-03-01 09:54:55 -0800

    update HOST on edge

--- config/environments/edge.rb
+++ config/environments/edge.rb
@@ -47,5 +47,5 @@ Songs::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  HOST = "behind.detourlab.com"
+  HOST = "song.detourlab.com"
 end
