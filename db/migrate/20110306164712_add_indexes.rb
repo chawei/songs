7aec8c86806e995809cb59105cca343a0df648b3
David Chawei Hsu <chawei.hsu@gmail.com>
2011-03-06 09:35:53 -0800

    temp fix

--- db/migrate/20110306164712_add_indexes.rb
+++ db/migrate/20110306164712_add_indexes.rb
@@ -1,9 +1,9 @@
 class AddIndexes < ActiveRecord::Migration
   def self.up
-    add_index :authorizations, :user_id
-    add_index :notes, :song_id
-    add_index :notes, :created_by_id
-    add_index :videos, :song_id
+    #add_index :authorizations, :user_id
+    #add_index :notes, :song_id
+    #add_index :notes, :created_by_id
+    #add_index :videos, :song_id
     add_index :relationships, [:source_type, :source_id, :relationship_type], :name => "rel_source_type_index"
     add_index :relationships, [:target_id, :target_type, :relationship_type], :name => "rel_target_type_index"
   end
